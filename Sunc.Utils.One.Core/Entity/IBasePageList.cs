5a78ae200a2bbee46b0d04e9a88eec139a30da16
aaaa4444ss22 <1058154599@qq.com>
2019-11-28 22:57:06 +0800

    新增分页类

---
+++ Sunc.Utils.One.Core/Entity/IBasePageList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunc.Utils.One.Core.Entity
+{
+    public interface IBasePageList<T>
+    {
+        IPageList<T> GetEntityPageList<Order>(Expression<Func<T, bool>> whereFunc
+            , Expression<Func<T, Order>> orderFunc
+            , int pageIndex = 1
+            , int pageSize = 10, bool isAsc = true);
+    }
+}
